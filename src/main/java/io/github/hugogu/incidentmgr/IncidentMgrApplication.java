d38a7fd42259d02ded18a4fcc19da2fb76260d07
Hugo Gu <hugogu@outlook.com>
2024-11-01 13:56:06 +0800

    Initialize project with Spring Boot Initializer.

---
+++ src/main/java/io/github/hugogu/incidentmgr/IncidentMgrApplication.java
@@ -0,0 +1,13 @@
+package io.github.hugogu.incidentmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentMgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentMgrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/hugogu/incidentmgr/IncidentMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hugogu.incidentmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentMgrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
